8e87b9ae510f2c2ca72ebb91bd5e8baa7c6798a2
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-06-29 08:13:38 -0300

    first commit

---
+++ src/main/java/me/dio/Santander_deve_week/SantanderDeveWeekApplication.java
@@ -0,0 +1,13 @@
+package me.dio.Santander_deve_week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDeveWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDeveWeekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/Santander_deve_week/SantanderDeveWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.Santander_deve_week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDeveWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
